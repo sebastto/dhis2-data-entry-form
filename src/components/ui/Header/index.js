b5eb57b022bf35b28c5b66d9991edf2e22f2e476
Sebastian Tangen Olsen <sebastto@student.matnat.uio.no>
2019-10-09 22:24:24 +0200

    Added app header component

---
+++ src/components/ui/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'React'
+
+import './index.css'
+
+const Header = ({title, subtitle}) => (
+    <div className="app-header">
+        <h2 className="app-header-title">
+            {title}
+        </h2>
+        <h3 className="app-header-subtitle">
+            {subtitle}
+        </h3>
+    </div>
+)
+
+export default Header

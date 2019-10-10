9bd6a430f33a4ced1e6ec9da5e5747c4791fe1b9
Vegard Bjørsvik <vegardbjr@gmail.com>
2019-10-10 22:06:55 +0200

    Prettified the src code

--- src/components/ui/DataEntryBox/index.js
+++ src/components/ui/DataEntryBox/index.js
@@ -1,32 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './index.css'
-import {Card} from '@dhis2/ui-core'
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { Card } from "@dhis2/ui-core";
 
-const DataEntryBox = (props) => {
+const DataEntryBox = props => {
+  const [title, setTitle] = useState(["ERROR"]);
+  const [date, setDate] = useState(["ERROR"]);
+  const [color, setColor] = useState(["red"]);
 
-    const [title, setTitle] = useState(["ERROR"]);
-    const [date, setDate] = useState(["ERROR"]);
-    const [color, setColor] = useState(["red"]);
+  useEffect(() => {
+    setTitle(props.title);
+    setDate(props.date);
+    setColor(props.color);
+  }, [props]);
 
-
-    useEffect(() => {
-       setTitle(props.title);
-       setDate(props.date);
-       setColor(props.color);
-    }, [props]);
-
-    
-    return (
-        <Card className="datacard">
-            <div className="dataentrybox" onClick={props.clickprop}>
-                <div className="colormark" style={{ background: color }}></div>
-                <p className="titlebox">{title}</p>
-                <p className="datebox">{date}</p>
-            </div>
-        </Card>
-    );
-    
-
-}
+  return (
+    <Card className="datacard">
+      <div className="dataentrybox" onClick={props.clickprop}>
+        <div className="colormark" style={{ background: color }}></div>
+        <p className="titlebox">{title}</p>
+        <p className="datebox">{date}</p>
+      </div>
+    </Card>
+  );
+};
 
 export default DataEntryBox;
--- src/components/ui/FacilityCard/index.js
+++ src/components/ui/FacilityCard/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import {Chip} from '@dhis2/ui-core'
+import React from "react";
+import { Chip } from "@dhis2/ui-core";
 
-import './index.css'
+import "./index.css";
 
-const FacilityCard = ({title, deadlines, onClick}) => {
-    return (
-        <button className="facility-card" onClick={onClick}>
-            <span className="facility-card-title">{title}</span>
-            <span className="facility-card-deadlines">
-                {(deadlines.expired > 0) ?
-                    <Chip className="chip-expired">{deadlines.expired}</Chip> : ""}
-                {(deadlines.due > 0) ?
-                    <Chip className="chip-due">{deadlines.due}</Chip> : ""}
-            </span>
-
-        </button>
-    );
+const FacilityCard = ({ title, deadlines, onClick }) => {
+  return (
+    <button className="facility-card" onClick={onClick}>
+      <span className="facility-card-title">{title}</span>
+      <span className="facility-card-deadlines">
+        {deadlines.expired > 0 ? (
+          <Chip className="chip-expired">{deadlines.expired}</Chip>
+        ) : (
+          ""
+        )}
+        {deadlines.due > 0 ? (
+          <Chip className="chip-due">{deadlines.due}</Chip>
+        ) : (
+          ""
+        )}
+      </span>
+    </button>
+  );
 };
 
 export default FacilityCard;

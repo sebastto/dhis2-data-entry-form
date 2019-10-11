2c57fe72336d0af5b56c8fdc501104c4fb6f46c3
Vegard Bjørsvik <vegardbjr@gmail.com>
2019-10-11 16:08:31 +0200

    Prettier the project with the new config

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { DataQuery } from "@dhis2/app-runtime";
-import i18n from "@dhis2/d2-i18n";
+import React from 'react';
+import { DataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
 
-import { TabBar, Tab } from "@dhis2/ui-core";
-import AppHeader from "./components/ui/AppHeader";
-import DataEntryBox from "./components/ui/DataEntryBox";
+import { TabBar, Tab } from '@dhis2/ui-core';
+import AppHeader from './components/ui/AppHeader';
+import DataEntryBox from './components/ui/DataEntryBox';
 
-import "./App.css";
+import './App.css';
 
 const query = {
-  me: {
-    resource: "me"
-  }
+    me: {
+        resource: 'me',
+    },
 };
 
 const MyApp = () => (
-  <>
-    <AppHeader title="Form overview" subtitle="Bum Kaku MCHP" />
-    <TabBar>
-      <Tab>All</Tab>
-      <Tab selected>Due soon</Tab>
-      <Tab>Completed</Tab>
-      <Tab>Expired</Tab>
-    </TabBar>
-    <div className="container">
-      <style jsx>{`
+    <>
+        <AppHeader title="Form overview" subtitle="Bum Kaku MCHP" />
+        <TabBar>
+            <Tab>All</Tab>
+            <Tab selected>Due soon</Tab>
+            <Tab>Completed</Tab>
+            <Tab>Expired</Tab>
+        </TabBar>
+
+        <div className="container">
+            <style jsx>{`
             .container {
                 position: absolute;
                 top: 48px;
@@ -38,36 +39,44 @@ const MyApp = () => (
                 font-size: 1rem;
             }
         }`}</style>
-      <DataQuery query={query}>
-        {({ error, loading, data }) => {
-          if (error) return <span>ERROR</span>;
-          if (loading) return <span>...</span>;
-          return (
-            <>
-              <h1>{i18n.t("Hello {{name}}", { name: data.me.name })}</h1>
-              <h3>{i18n.t("Welcome to DHIS2!")}</h3>
-              <DataEntryBox
-                title="Child Health"
-                date="24.11"
-                color="blue"
-                clickprop={() =>
-                  console.log("forward to Data Entry with form_id")
-                }
-              ></DataEntryBox>
-              <DataEntryBox
-                title="A Very very very very very very very very very very very very long form"
-                date="31.12"
-                color="red"
-                clickprop={() =>
-                  console.log("forward to Data Entry with form_id")
-                }
-              ></DataEntryBox>
-            </>
-          );
-        }}
-      </DataQuery>
-    </div>
-  </>
+            <DataQuery query={query}>
+                {({ error, loading, data }) => {
+                    if (error) return <span>ERROR</span>;
+                    if (loading) return <span>...</span>;
+                    return (
+                        <>
+                            <h1>
+                                {i18n.t('Hello {{name}}', {
+                                    name: data.me.name,
+                                })}
+                            </h1>
+                            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+                            <DataEntryBox
+                                title="Child Health"
+                                date="24.11"
+                                color="blue"
+                                clickprop={() =>
+                                    console.log(
+                                        'forward to Data Entry with form_id'
+                                    )
+                                }
+                            ></DataEntryBox>
+                            <DataEntryBox
+                                title="A Very very very very very very very very very very very very long form"
+                                date="31.12"
+                                color="red"
+                                clickprop={() =>
+                                    console.log(
+                                        'forward to Data Entry with form_id'
+                                    )
+                                }
+                            ></DataEntryBox>
+                        </>
+                    );
+                }}
+            </DataQuery>
+        </div>
+    </>
 );
 
 export default MyApp;

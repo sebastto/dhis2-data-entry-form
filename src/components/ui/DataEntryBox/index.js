2cad6616ce22f22efe1aa87ff6af4af38914311f
sebastto <sebastto@ifi.uio.no>
2019-10-23 21:35:05 +0200

    feat(see-edit-form): added animation

--- src/components/ui/DataEntryBox/index.js
+++ src/components/ui/DataEntryBox/index.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react'
 import './index.css'
 import PropTypes from 'prop-types'
 import { Card } from '@dhis2/ui-core'
+import Collapse from '@material-ui/core/Collapse'
+import ViewIcon from '../../icons/ViewIcon'
+import EditIcon from '../../icons/EditIcon'
 
 const DataEntryBox = props => {
     const [title, setTitle] = useState(['ERROR'])
     const [date, setDate] = useState(['ERROR'])
     const [color, setColor] = useState(['red'])
+    const [collapsed, setCollapsed] = useState(false)
 
     useEffect(() => {
         setTitle(props.title)
@@ -15,11 +19,30 @@ const DataEntryBox = props => {
     }, [props])
 
     return (
-        <Card className="datacard">
-            <div className="dataentrybox box-shadow" onClick={props.clickprop}>
-                <div className="colormark" style={{ background: color }} />
-                <p className="titlebox">{title}</p>
-                <p className="datebox">{date}</p>
+        <Card className="datacard box-shadow">
+            <div className="datacard-colormark" style={{ background: color }} />
+            <div
+                className="datacard-content"
+                onClick={() => setCollapsed(!collapsed)}
+            >
+                <div className="datacard-content-info">
+                    <p className="titlebox">{title}</p>
+                    <p className="datebox">{date}</p>
+                </div>
+
+                <Collapse in={collapsed}>
+                    <div className="data-card-action-container">
+                        <div className="datacard-icon-group">
+                            <ViewIcon />
+                            <p className="datacard-icon-group-text">View</p>
+                        </div>
+
+                        <div className="datacard-icon-group">
+                            <EditIcon />
+                            <p className="datacard-icon-group-text">Edit</p>
+                        </div>
+                    </div>
+                </Collapse>
             </div>
         </Card>
     )

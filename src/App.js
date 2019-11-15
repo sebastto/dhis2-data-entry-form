9e6f8522aa52a935b261a9975a17c2decd28e25d
tolgaz <tolgaz@uio.no>
2019-11-15 23:58:28 +0100

    feat: somewhat errorhandling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Tab, TabBar } from '@dhis2/ui-core'
+import { Tab, TabBar, AlertBar } from '@dhis2/ui-core'
 import { useDataEngine } from '@dhis2/app-runtime'
 import classNames from 'classNames'
 import reactRouterDom from 'react-router-dom'
@@ -33,18 +33,25 @@ const App = () => {
     const [selectedFacility, setSelectedFacility] = useState(null)
     const [facilities, setFacilities] = useState(undefined)
     const [mobileActiveTab, setMobileActiveTab] = useState(FACILITIES)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        getAllOrganisationData(engine).then(({ organisations }) => {
-            const facilities = processDataSets(
-                organisations,
-                getCompleteFormEngine
-            )
-            setFacilities(facilities)
-            if (facilities.length === 1) {
-                setMobileActiveTab(FORMS)
-            }
-        })
+        setError(null)
+        try {
+            getAllOrganisationData(engine).then(({ organisations }) => {
+                const facilities = processDataSets(
+                    organisations,
+                    getCompleteFormEngine
+                )
+                setFacilities(facilities)
+                if (facilities.length === 1) {
+                    setMobileActiveTab(FORMS)
+                }
+            })
+        } catch (e) {
+            console.error(e)
+            setError(['error', e])
+        }
     }, [])
 
     useEffect(() => {
@@ -61,8 +68,68 @@ const App = () => {
         }
     }, [facilities])
 
+    const getAlertBar = () => {
+        let doneOne = false
+        if (facilities) {
+            if (facilities.length === 0) {
+                doneOne = true
+                return (
+                    <AlertBar duration={2000} warning>
+                        No facilities were found for this user.
+                    </AlertBar>
+                )
+            } else if (facilities.length === 1) {
+                return (
+                    <AlertBar duration={4000}>
+                        Only one facility was found for this user.
+                    </AlertBar>
+                )
+            }
+        }
+        if (selectedFacility && !doneOne) {
+            selectedFacility.dataSets = []
+            if (selectedFacility.dataSets.length === 0) {
+                return (
+                    <AlertBar info duration={6000}>
+                        This facility has no forms!
+                    </AlertBar>
+                )
+            }
+        }
+        if (error) {
+            return (
+                <AlertBar warning>
+                    Error occurred while retrieving data. The application might
+                    still be usable.
+                </AlertBar>
+            )
+        }
+    }
+
     return (
         <>
+            <div
+                className="alert-bars"
+                style={{
+                    bottom: '100px',
+                    width: '100%',
+                    padding: '0 16px',
+                    position: 'fixed',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    zIndex: '9999',
+                }}
+            >
+                {facilities && (
+                    <AlertBar duration={4000} icon success>
+                        All information has been cached. The application is
+                        available offline.
+                    </AlertBar>
+                )}
+                {(facilities || selectedFacility || error) && getAlertBar()}
+            </div>
+
             <div
                 className={classNames('app-container', {
                     'tab-view': !desktopView,
